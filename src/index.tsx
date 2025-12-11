9f0a6461df295a296a01ef5b325195fe40c11021
Rusticdoom <ruslan.dostovalov@gmail.com>
2025-12-11 01:26:57 -0800

    feat: layout components have been added

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/scss/style.scss";
 import "./assets/scss/normalize.scss";
-import { ToDoList } from "./pages/ToDoList";
+import { ToDoListPage } from "./pages/ToDoListPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoList />
+    <ToDoListPage />
   </React.StrictMode>
 );
